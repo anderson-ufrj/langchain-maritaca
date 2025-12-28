14d1b068e27551f7009596089eeec43387651521
anderson-ufrj <anderson-ufrj@users.noreply.github.com>
2025-12-28 06:22:30 -0300

    feat(exports): export callback handlers from package
    Export CostTrackingCallback, LatencyTrackingCallback,
    TokenStreamingCallback, and CombinedCallback from main package.

--- langchain_maritaca/__init__.py
+++ langchain_maritaca/__init__.py
@@ -7,8 +7,22 @@ Location: Minas Gerais, Brasil
 GitHub: https://github.com/anderson-ufrj
 """
 
+from langchain_maritaca.callbacks import (
+    CombinedCallback,
+    CostTrackingCallback,
+    LatencyTrackingCallback,
+    TokenStreamingCallback,
+)
 from langchain_maritaca.chat_models import ChatMaritaca
 from langchain_maritaca.embeddings import DeepInfraEmbeddings
 from langchain_maritaca.version import __version__
 
-__all__ = ["ChatMaritaca", "DeepInfraEmbeddings", "__version__"]
+__all__ = [
+    "ChatMaritaca",
+    "CombinedCallback",
+    "CostTrackingCallback",
+    "DeepInfraEmbeddings",
+    "LatencyTrackingCallback",
+    "TokenStreamingCallback",
+    "__version__",
+]
